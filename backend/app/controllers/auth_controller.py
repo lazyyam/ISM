c2f4ba48aae8761578864b51a9c865535d4090f5
darren124 <darrenleong124@gmail.com>
2025-04-03 12:24:06 +0800

    test push

--- backend/app/controllers/auth_controller.py
+++ backend/app/controllers/auth_controller.py
@@ -56,10 +56,13 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
 # User Login
 @auth_router.post("/login")
 def login(user: UserLogin, db: Session = Depends(get_db)):
-    
+
     db_user = db.query(User).filter(User.email == user.email).first()
     if not db_user or not verify_password(user.password, db_user.password):
         raise HTTPException(status_code=401, detail="Invalid email or password")
     
     token = create_access_token({"sub": db_user.email}, timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
     return {"access_token": token, "token_type": "bearer"}
+
+
+#testing
