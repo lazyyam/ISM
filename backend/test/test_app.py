6e0c5049d0819072381d5ff4f09f9c2f605dbb12
lazyyam <yyzhan020623@gmail.com>
2025-07-05 18:18:29 +0800

    testing sonarqube

---
+++ backend/test/test_app.py
@@ -0,0 +1,11 @@
+# backend/test/test_app.py
+
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "FastAPI is running"}
